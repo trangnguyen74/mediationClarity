a0e265b52179a7c74dc64dfd9082dba9607a5f6d
Trang Nguyen <nqtrang.hanoi@gmail.com>
2022-02-01 00:58:13 -0500

    rename from R files

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -12,6 +12,8 @@
 #   Install Package:           'Cmd + Shift + B'
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
+
+#' @noRd
 
 hello <- function() {
   print("Hello, world!")
